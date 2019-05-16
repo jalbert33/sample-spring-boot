6645672707fef338a84532994366d6783ee1c896
User <soufiane@exemple.com>
2019-05-16 10:31:46 +0200

    modify

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -13,4 +13,45 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 	}
 
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+		logger.info("Hello World!");
+        	assertTrue( true );
+    }
+    public Integer additionner(Integer a, Integer b) 
+    {
+	Integer somme;
+	somme = a + b;
+	return somme;
+    }
+    @Test
+    public void additionner_integer_should_return_an_integer() 
+    {
+     Integer somme = additionner(2,3);
+     if (somme == 5) {
+  System.out.println("test OK");
+    		    } else {
+  System.out.println("test KO");
+                    }
+    }		
+   public Integer soustraire(Integer a, Integer b) 
+    {
+	Integer somme;
+	somme = a - b;
+	return somme;
+    }	
+    @Test
+    public void soustraire_integer_should_return_an_integer() 
+    {
+     Integer somme = soustraire(3,3);
+     if (somme == 0) {
+  System.out.println("test OK");
+    		    } else {
+  System.out.println("test KO");
+  assertTrue( false );
+                    }
+    }	
+
+
 }
